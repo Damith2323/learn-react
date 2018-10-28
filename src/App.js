acad5d03ed89e50932b233aac2e5d7e3a1f558a4
Damith <damith94sliit@gmail.com>
2018-10-28 10:38:27 +0530

    T14 - Outputting Lists

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import Ninjas from "./Ninjas";
 
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Raptor", age: 24, belt: "Black", id: 1 },
+      { name: "Mia", age: 22, belt: "Red", id: 2 },
+      { name: "Hype", age: 25, belt: "Black", id: 3 }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>My first React App</h1>
         <p>Welcome</p>
-        <Ninjas name="Mia" age="22s" belt="black" />
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
