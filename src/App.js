361705a018b17e300e576808f5b42bd10a628f22
Damith <damith94sliit@gmail.com>
2018-10-28 23:34:42 +0530

    T18 - Functions as Props - half done

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ class App extends Component {
       { name: "Hype", age: 25, belt: "Black", id: 3 }
     ]
   };
+  addNinja = ninja => {
+    console.log(ninja);
+  };
   render() {
     return (
       <div className="App">
         <h1>My first React App</h1>
         <p>Welcome</p>
         <Ninjas ninjas={this.state.ninjas} />
-        <AddNinja />
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
