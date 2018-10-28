c6cf8761847c476b0d7a76a90086a01d1c890e51
Damith <damith94sliit@gmail.com>
2018-10-28 11:39:36 +0530

    T16 - conditional-ternary

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -2,17 +2,13 @@ import React from "react";
 
 const Ninjas = ({ ninjas }) => {
   const ninjaList = ninjas.map(ninja => {
-    if (ninja.age > 22) {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>Name: {ninja.name}</div>
-          <div>Age: {ninja.age}</div>
-          <div>Belt: {ninja.belt}</div>
-        </div>
-      );
-    } else {
-      return null;
-    }
+    return ninja.age > 22 ? (
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
+      </div>
+    ) : null;
   });
   return <div className="ninja-list">{ninjaList}</div>;
 };
